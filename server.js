bc84c16becf617600359bb2dbe8dcc015c5c00bd
Oliver Atom <j_hernandez50@hotmail.com>
2021-09-04 21:57:19 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,58 @@
+
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let notelength = (noteList.length).toString();
+
+    newNote.id = notelength;
+    noteList.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+    res.json(noteList);
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteId = (req.params.id).toString();
+
+    noteList = noteList.filter(selected =>{
+        return selected.id != noteId;
+    })
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+    res.json(noteList);
+});
+
+
+app.listen(PORT, () => console.log("Server listening on port " + PORT));
